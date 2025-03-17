e46e1c817cb40bd582e48576c65d3e1357d3f2c3
sejin_k6 <white14570@naver.com>
2025-03-17 14:26:56 +0900

    upload

---
+++ calculate.py
@@ -0,0 +1,40 @@
+# 사칙연산 함수 정의
+def plus(a, b):
+    return a + b
+
+def minus(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+if __name__ == "__main__":
+    # 사용자 입력
+    print('\n첫번째 숫자를 입력하세요.')
+    input1 = float(input('입력: '))
+
+    print('\n원하는 사칙연산 기호 중 하나를 선택하세요. (+, -, *, /)')
+    act = input('기호: ')
+
+    print('\n두번째 숫자를 입력하세요.')
+    input2 = float(input('입력: '))
+
+    # 연산 수행
+    if act == '+':
+        result = plus(input1, input2)
+    elif act == '-':
+        result = minus(input1, input2)
+    elif act == '*':
+        result = mul(input1, input2)
+    elif act == '/':
+        if input2 != 0:
+            result = divide(input1, input2)
+        else:
+            result = "0으로 나눌 수 없습니다."
+    else:
+        result = "잘못된 연산 기호입니다."
+
+    print(f'\n사칙연산 결과는 {result}입니다.')
